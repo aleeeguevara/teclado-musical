3efc41c5059ab81ccfea3b6ac12afceec05782ff
Alexandra <bguevaraale@gmail.com>
2021-11-24 14:01:10 -0300

    Reproduzir sons do teclado musical ao pressionar teclas Enter e Space

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-const tocaSom = (nomeID) => {
-  document.querySelector(nomeID).play();
+const tocaSom = (seletorAudio) => {
+  const elemento = document.querySelector(seletorAudio);
+  
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  }else{
+    console.log("Elemento não encontrado ou seletor inválido");
+  }
 };
 
-const teclas= document.querySelectorAll('.tecla');
+const teclas = document.querySelectorAll(".tecla");
 
 teclas.forEach((tecla, index) => {
-    tecla.addEventListener("click", () =>{
-        tocaSom(`#som_${teclas[index].classList[1]}`); 
-    })
-    
-})
+  tecla.addEventListener("click", () => {
+    tocaSom(`#som_${teclas[index].classList[1]}`);
+  });
+  tecla.addEventListener("keydown", (event) => {
+    if (event.keyCode === 13 || event.keyCode === 32) {
+      tecla.classList.add("ativa");
+    }
+  });
+  tecla.addEventListener("keyup", () => {
+    tecla.classList.remove("ativa");
+  });
+});
+
